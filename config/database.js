85aa369cb9aae61f83295b0d1a4ddae807b1ed6a
terry-source <yuan.terry@gmail.com>
2024-01-24 16:52:47 +1100

    express gen and model connection

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// contect mongodb
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL,);
+
+// create mongoose connection object
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
---
+++ models/flight.js
@@ -0,0 +1,35 @@
+// import mongoose, create schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+    },
+
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+    },
+
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+
+    departs: {
+        type: Date,
+        default: function () {
+            const date = new Date();
+            const year = date.getFullYear() + 1;
+            date.setFullYear(year);
+            return date;
+        },
+    },
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
