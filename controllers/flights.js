31662e0b9ec1954d28fe708ba0708ac66471b386
terry-source <pendalterry@gmail.com>
2024-01-25 16:18:17 +1100

    add show

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,7 @@
-const Flights = require("../models/flight");
+const Flight = require("../models/flight");
 
 async function index(req, res, next) {
-  const flights = await Flights.find({});
+  const flights = await Flight.find({});
 
   res.render("index", {
     title: "All Flights",
@@ -10,10 +10,35 @@ async function index(req, res, next) {
 }
 
 function newFlight(req, res) {
-  res.render("flights/new");
+  res.render("flights/new", { title: "Add New Flight", errorMsg: "" });
+}
+
+async function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === "") delete req.body[key];
+  }
+
+  try {
+    await Flight.create(req.body);
+    res.redirect("/flights", { title: "All Flights" });
+  } catch (err) {
+    console.log(err);
+    res.render("flights/new", {
+      title: "Add New Flight",
+      errorMsg: err.message,
+    });
+  }
+  s;
+}
+
+async function show(req, res) {
+  const flight = await Flight.findById(req.params.id);
+  res.render("flights/show", { title: "Flight Detail", flight });
 }
 
 module.exports = {
   index,
   new: newFlight,
+  create,
+  show,
 };
--- routes/flights.js
+++ routes/flights.js
@@ -5,4 +5,8 @@ const flightsCtrl = require("../controllers/flights");
 /* GET flights listing. */
 router.get("/new", flightsCtrl.new);
 
+router.post("/", flightsCtrl.create);
+
+router.get("/:id", flightsCtrl.show);
+
 module.exports = router;
